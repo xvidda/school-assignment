b1cdba6d80d378998ad19b68da4c100f3ea07faa
daniel <lildeado99@gmail.com>
2023-10-19 00:10:48 +0100

    my one commit

---
+++ upload.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Upload Page</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class="container">
+        <h2>Upload Study Material Page</h2>
+        <form id="loginForm">
+            <label for="title">Title:</label>
+            <input type="text" id="title" placeholder="Title" name="title" required>
+
+            <label for="Description">Description:</label>
+            <input type="text" id="Description" placeholder="Description" name="Description" required>
+
+            <label for="File">File:</label>
+            <input type="file" id="File" name="File" required>
+
+            <button type="submit">submit</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>view page</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+       
+        <h1 style="text-align: center;">Study Materials Page</h1>
+       
+        <table border="1">
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Date Uploaded</th>
+            </tr>
+            <tr>
+                <td>Web Designing </td>
+                <td>Web design refers to the process of creating the visual and interactive elements of websites. </td>
+                <td>2023-9-27</td>
+            </tr>
+            <tr>
+                <td>Robotics</td>
+                <td>Robotics is a multidisciplinary field that involves the design, construction, operation, and use of robots</td>
+                <td>2023-10-18</td>
+            </tr>
+        </table>
+   
+    </body>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
